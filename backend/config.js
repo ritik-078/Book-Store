9f28a3ddf453ab487b5c407e41caf27c5da6bb9b
ritik-078 <ritikbhateley125@gmail.com>
2023-12-29 16:45:40 +0530

    Database Connection established

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,6 @@
 const PORT = 5000
-
-module.exports = PORT
+const mongoURL = 
+    'mongodb+srv://ritikbhateley125:SSlhuDil7lWFdnvP@bookstore.oezdch2.mongodb.net/?retryWrites=true&w=majority'
+    
+module.exports.PORT = PORT 
+module.exports.mongoURL = mongoURL
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
-const PORT = require('./config')
+const mongoose = require('mongoose')
+const {PORT, mongoURL} = require('./config')
 
 
 app.get('/', (request,response) => {
     response.end('Home Page')
 })
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-});
+
+mongoose.connect(mongoURL)
+    .then(() => {
+        console.log("Connected to Database")
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`)
+        });
+    })
+    .catch((err) => {
+        console.log(err)
+    })
